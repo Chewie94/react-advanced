df275463826dd9183f816ae506d34a828e6b1a81
Chewie94 <mykhailo.butkevych@gmail.com>
2023-11-14 19:48:09 +0400

    feat: part 2 finished, decompose webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,36 +1,21 @@
-import path from 'path';
 import webpack from 'webpack';
-import HTMLWebpackPlugin from "html-webpack-plugin";
+import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
+import {BuildPaths} from "./config/build/types/config";
+import path from "path";
 
-const config: webpack.Configuration = {
-    mode: 'development',
+const paths: BuildPaths = {
     entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, 'build'),
-        clean: true
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-          // Use HTML from the public folder as a template. Scripts will be injected into this file dynamically during the build process. Ensure that this template serves as the foundation for incorporating dynamic content and functionality.
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        // Specify the loaders and options for processing different types of files
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ],
-    },
-    // No need to specify extensions for next imports:
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
 }
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+    mode: 'development',
+    paths,
+    isDev,
+})
 
 export default config;
