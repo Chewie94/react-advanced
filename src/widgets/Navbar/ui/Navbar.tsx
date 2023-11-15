28b223fa8c07e02857ce4877cd6cf68aaaed88b6
Chewie94 <mykhailo.butkevych@gmail.com>
2023-11-15 23:40:27 +0400

    feat: 2-13, svg & files loader, button, refactor

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -10,12 +11,13 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames (cls.navbar, {}, [className])}>
+            <ThemeSwitcher />
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>
-                    Главная
+                    Home
                 </AppLink>
                 <AppLink theme={AppLinkTheme.RED} to={'/about'}>
-                    О сайте
+                    About
                 </AppLink>
             </div>
         </div>
